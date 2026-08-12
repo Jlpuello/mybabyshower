7bcb9212a9748e360887d71457d790801494aa19
Jlpuello <puelloconeo23@hotmail.com>
2026-08-11 23:34:17 -0500

    produccion01

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,6 +4,15 @@ import { RouterProvider } from 'react-router-dom'
 import './styles/index.css'
 import { router } from './router'
 
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader')
+  if (preloader) {
+    setTimeout(() => {
+      preloader.classList.add('fade-out')
+    }, 2000)
+  }
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
